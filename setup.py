7c5b7b539523cd62b938c78f041aed64f4a5c97c
Dann Albright <dann.albright@gmail.com>
2023-05-31 12:17:21 -0600

    Adding project directory.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Modeling whether educational outcomes affect COVID vaccine hesitancy, hospitalization,s, and/or deaths.',
+    author='Dann Albright',
+    license='MIT',
+)
